c5a8e418f3f54681117b4d309efbbbd7342523b5
bohdanP <bohdan.psh@gmail.com>
2016-04-23 16:28:41 +0300

    авысч

--- Demo/js/common.js
+++ Demo/js/common.js
@@ -1,20 +1,22 @@
 
 
 $(document).ready(function () {
-	$('.sub > a').click(function(){
-		if ($(this).attr('class') != 'active'){
-			$('.sub ul').slideUp();
-			$(this).next().slideToggle();
-		}
-		return false;
-	});
-	$('.mini-menu > ul > li > a').click(function(){
-		$('.mini-menu > ul > li > a, .sub a').removeClass('active');
-		$(this).addClass('active');
+	 $('.sub > a').click(function(){
+      $('.sub ul').slideUp();
+       if ($(this).next().is(":visible")){
+           $(this).next().slideUp();
+       } else {
+       $(this).next().slideToggle();
+       }
+      return false;
+    });
+       $('.mini-menu > ul > li > a').click(function(){
+	   $('.mini-menu > ul > li > a, .sub a').removeClass('active');
+	   $(this).addClass('active');
 	}),
-	$('.sub ul li a').click(function(){
-		$('.sub ul li a').removeClass('active');
-		$(this).addClass('active');
+       $('.sub ul li a').click(function(){
+	   $('.sub ul li a').removeClass('active');
+	   $(this).addClass('active');
 	});
 });
 
