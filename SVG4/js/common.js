db9241ef07c005a920c0345340aeba7e74d38281
bohdanP <bohdan.psh@gmail.com>
2016-05-06 14:51:53 +0300

    fdg

---
+++ SVG4/js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	setTimeout(function() {$("#p2").attr("class","path"); $("#t1").attr("class","text");}, 500);
+	setTimeout(function() {$("#p3").attr("class","path");}, 700);
+	setTimeout(function() {$("#p4").attr("class","path"); $("#t2").attr("class","text");}, 2500);
+	setTimeout(function() {$("#p5").attr("class","path"); $("#t3").attr("class","text");}, 3200);
+	
+
+});
