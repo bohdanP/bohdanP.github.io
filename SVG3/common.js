f3b09de8fb72d7724a4558ef1272d78c69f50bd6
bohdanP <bohdan.psh@gmail.com>
2016-05-08 23:16:18 +0300

    gkh

---
+++ SVG3/common.js
@@ -0,0 +1,6 @@
+$(document).ready( function() {
+
+	TweenMax.to($('#bgpoly'), 6, {css:{transform: "translateX(-1200px)"}, repeat:-1, ease:Linear.easeNone});
+	TweenMax.to($('#bgpoly2'), 6, {css:{transform: "translateX(0px)"}, repeat:-1, ease:Linear.easeNone});
+	
+});
